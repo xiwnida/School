eb4ac670a3a776cd47fa9876e3dba9f7f74d4dc4
pupil <pupil@ivkhk.local>
2018-11-29 11:40:35 +0200

    всякая хрень

---
+++ JKTVR18-JS/public_html/Array_School/school.js
@@ -0,0 +1,57 @@
+/* 
+ * Задание:
+ * создать массив список студентов: имя, группа, возраст, адрес - город, улица, дом кв.
+ * Вывести на страницу index.html
+ * написать функцию печати
+ * 
+ * дополнительно
+ * добавить фоно не для всех студентов
+ */
+
+
+var students=[
+    {
+        name:'Anna',
+        group:'JKTVR18',
+        age:20,
+        photo:'1.jpg',
+        adress:{
+            citi:'K-Jarve',
+            street:'Puru tee',
+            house:'12-2'
+        }
+    },
+    {
+        name:'Mihail',
+        group:'JKTVR18',
+        age:22,
+        photo:'3.jpg',
+        adress:{
+            citi:'Narva',
+            street:'Mante tee',
+            house:'21-5'
+        }
+    },
+    {
+        name:'Kirill',
+        group:'JKTVR18',
+        age:31,
+        photo:'4.jpg',
+        adress:{
+            citi:'Johvi',
+            street:'Sopruse',
+            house:'11-1'
+        }
+    },
+    {
+        name:'Inna',
+        group:'JKTVR18',
+        age:17,
+        adress:{
+            citi:'K-Jarve',
+            street:'Kalevi',
+            house:'8'
+        }
+    }
+    
+];
