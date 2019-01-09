ae5bba7579d8ee0899292f48d2891ecfc859ae40
pupil <pupil@ivkhk.local>
2019-01-09 09:20:06 +0200

    питон

--- Python/class_VEERA.py
+++ Python/class_VEERA.py
@@ -1,3 +1,4 @@
+"""
 #1
 class Dog:
     age=0
@@ -55,3 +56,57 @@ class Person:
 bob=Person()
 bob.name='bob'
 print(bob.name, 'has', bob.money, 'dollars')
+
+"""
+
+#Повторение пройденного
+#1
+class Cat:
+    name=''
+    color=''
+    weight=''
+
+    def meow(self):
+        print(cat.name,'says "meow"')
+
+snow=Cat()
+snow.name='Снежок'
+snow.meow()
+
+
+#2
+class Address:
+    name=''
+    city=''
+    street=''
+    house=''
+
+    def printAdress(self):
+        print(self.name, 'проживает по адресу:',self.city, self.street, self.house)
+
+my_adress=Address()
+my_adress.name='Вера'
+my_adress.city='Kohtla-Jarve'
+my_adress.street='Raudtee'
+my_adress.house='29'
+
+my_adress.printAdress()
+
+#3
+class Monster:
+    def __init__(self, name, health=100):
+        self.name=name
+        self.health=health
+
+    def decreaseHealth(self, amount):
+        self.health-=amount
+        if self.health<=0:
+            print('Здоровье монстра "'+ str(self.name)+ '" опустилось до нуля.')
+            print(self.name, 'погиб')
+        else:
+            print('У монстра по имени "'+ str(self.name)+'" осталось '+ str(self.health)+' здоровья.')
+
+skelet=Monster('Скелет', 50)
+skelet.decreaseHealth(20)
+skelet.decreaseHealth(5)
+skelet.decreaseHealth(40)
