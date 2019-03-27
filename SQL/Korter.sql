d052058db0bbb8472336d61a56a68f704d6c71f4
pupil <pupil@ivkhk.ee>
2019-03-27 11:24:12 +0300

    сейвы

--- SQL/Korter.sql
+++ SQL/Korter.sql
@@ -16,16 +16,16 @@ CREATE DATABASE Korter_JKTVR18
 
 USE Korter_JKTVR18
 DELETE meter
+DROP TABLE meter
 CREATE TABLE meter(
 	meter_st FLOAT,
 	meter_end FLOAT,
-	meter_date DATE PRIMARY KEY,
+	meter_date DATE PRIMARY KEY DEFAULT EOMONTH (GETDATE()),
 	--enter_date DATETIME DEFAULT GETDATE())
 	enter_date DATETIME)
 	
-INSERT INTO meter VALUES
-	(100,200,'12/31/2018', '01/02/2019'),
-	(200,350,'1/31/2019', '02/02/2019')
+INSERT INTO meter (meter_st, meter_end, enter_date) VALUES
+	(0,100, '01/02/2019')
 
 SELECT * FROM meter
 
@@ -60,11 +60,8 @@ AS
 	END
 	END
 GO
+
 EXEC get_meter 485, '03/02/2019'
-EXEC get_meter 575, '04/02/2019'
-EXEC get_meter 675, '05/02/2019'
-EXEC get_meter 795, '06/02/2019'
-SELECT * FROM meter
 
 CREATE TABLE apartment(
 	ApartNum NVARCHAR(4) PRIMARY KEY,
@@ -73,56 +70,85 @@ CREATE TABLE apartment(
 	FIO NVARCHAR(50),
 	Phone NVARCHAR(12),
 	TypeHeat NVARCHAR(15),
-	ProsentSquare INT
+	ProsentSquare FLOAT
 	)
-DROP TABLE apartment
-SELECT * FROM apartment
 
 INSERT INTO apartment VALUES
-	('110', 20.5, 2, 'Veera Krjutskova', '55542221', N'Центральное', 100),
-	('111', 18, 3, 'Igor Fomkin', '1234567', N'Газ', 7),
-	('112', 15, 2, 'Vasja Pupkin', '7654321', N'Электро', 20),
-	('210', 17.3, 1, 'Sasha White', '2134663', N'Газ', 9),
-	('211', 16.5, 3,  'Grisha Black', '4256734', N'Центральное', 100),
-	('212', 20, 2, 'Lena Elena', '1345265', N'Электро', 15)
+	('110', 20.5, 2, 'Veera Krjutskova', '55542221', N'Центральное', 1),
+	('111', 18, 3, 'Igor Fomkin', '1234567', N'Газ', 0.07),
+	('112', 15, 2, 'Vasja Pupkin', '7654321', N'Электро', 0.2),
+	('210', 17.3, 1, 'Sasha White', '2134663', N'Газ', 0.09),
+	('211', 16.5, 3,  'Grisha Black', '4256734', N'Центральное', 1),
+	('212', 20, 2, 'Lena Elena', '1345265', N'Электро', 0.15)
 
 CREATE TABLE tarifHeat( --Тариф за отопление
 	Data DATE PRIMARY KEY DEFAULT EOMONTH (GETDATE()),
 	Tarif FLOAT
 	)
-DROP TABLE tarifHeat
 
 CREATE TABLE tarifSquare( -- Тариф за площадь
 	Data DATE PRIMARY KEY DEFAULT EOMONTH (GETDATE()),
 	Tarif FLOAT)
-DROP TABLE tarifSquare
 
 INSERT INTO tarifHeat (Tarif) VALUES
 	(52.30)
 
 INSERT INTO tarifSquare (Tarif) VALUES
 	(1.30)
-SELECT * FROM tarifSquare
 
+DROP TABLE schet
 CREATE TABLE schet(
 	Data DATE DEFAULT EOMONTH (GETDATE()),
 	ApartNum NVARCHAR(4),
+	SquareTarif FLOAT,
 	SquarePay FLOAT,
+	HeatTarif FLOAT,
 	HeatPay FLOAT,
-	PaySumm FLOAT)
-DROP TABLE schet
-	
+	PaySumm FLOAT,
+	PRIMARY KEY (Data, ApartNum))
+
+CREATE TABLE AllData(
+	Data DATE DEFAULT EOMONTH (GETDATE()) PRIMARY KEY,
+	HeatAllPay FLOAT, --Общая сумма за отопление
+	HeatOnePay FLOAT) --Одна доля за отопление
+
+DROP PROC get_schet
 CREATE PROC get_schet
 AS
 	BEGIN
-		DECLARE @HeatAll FLOAT
-		SET @HeatAll = (SELECT meter_end-meter_st FROM meter WHERE EOMONTH(enter_date) = EOMONTH(GETDATE())) --@HeatAll * (SELECT Tarif FROM tarifHeat WHERE Data = EOMONTH(GETDATE()))
-		
-		DECLARE @SquareAll FLOAT
-		SET @SquareAll = (SELECT SUM(ApartSquare) FROM apartment) --Рассчет всей площади квартир
-		DECLARE @AllSquarePayment FLOAT
-		SET @AllSquarePayment = @SquareAll * (SELECT Tarif FROM tarifSquare WHERE EOMONTH(Data) = EOMONTH(GETDATE())) -- Рассчет общей суммы за квартиры
-		
-		INSERT INTO schet (ApartNum)
-		SELECT apartNum,  FROM apartment
-		SELECT * FROM schet
+		DECLARE @meterData FLOAT
+		DECLARE @HeatPayment FLOAT
+
+		SET @meterData = (SELECT meter_end-meter_st FROM meter WHERE EOMONTH(meter_date) = EOMONTH(GETDATE())) --Данные счетчика
+		SET @HeatPayment = @meterData * (SELECT Tarif FROM tarifHeat WHERE Data = EOMONTH(GETDATE())) --Всего за отопление
+
+		INSERT INTO Alldata (HeatAllPay, HeatOnePay) VALUES
+			(@HeatPayment, @HeatPayment/100)
+
+		DECLARE @SquareTarif FLOAT
+		DECLARE @HeatTarif FLOAT
+
+		SET @SquareTarif = (SELECT Tarif FROM tarifSquare WHERE Data = EOMONTH(GETDATE()))
+		SET @HeatTarif = (SELECT Tarif FROM tarifHeat WHERE Data = EOMONTH(GETDATE()))
+
+		INSERT INTO schet (ApartNum, SquareTarif, SquarePay, HeatTarif, HeatPay, PaySumm)
+		SELECT apartNum, @SquareTarif, ROUND((apartSquare * @SquareTarif),2), @HeatTarif, ROUND((apartSquare * ProsentSquare * @HeatPayment/100),2),
+		ROUND((apartSquare * @SquareTarif),2) + ROUND((apartSquare * ProsentSquare * @HeatPayment/100),2)  FROM apartment
+	END
+GO
+
+DELETE alldata
+DELETE schet
+
+EXEC get_meter 190, '03/02/2019'
+EXEC get_schet
+
+SELECT * FROM alldata
+
+CREATE VIEW get_schet_view
+AS
+SELECT s.Data, s.ApartNum, a.FIO, a.Phone, s.SquareTarif AS Тариф_площади, s.SquarePay AS Оплата_площади, s.HeatTarif AS Тариф_отопления, s.HeatPay AS Оплата_отопления, s.PaySumm AS К_оплате_всего
+FROM schet s JOIN apartment a ON s.ApartNum = a.ApartNum
+WHERE s.Data = EOMONTH(GETDATE());
+
+SELECT * FROM get_schet_view --Посмотреть представление счета
